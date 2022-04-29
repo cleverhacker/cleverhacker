bc61172597ad055d33343072669dfea8145beecb
cleverhacker <anonymous.nakum14@gmail.com>
2022-04-29 17:08:55 +0000

    new file:   buffer.java

---
+++ buffer.java
@@ -0,0 +1,7 @@
+public class buffer {
+    public static void main(String[] args) {
+        StringBuffer sc = new StringBuffer("xssss");
+        sc.append("sdgfdsg");
+        System.out.println(sc);
+    }
+}
